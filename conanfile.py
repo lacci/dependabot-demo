23fec1aa96cd4ab6413a522fb0bc54dbfecdc6d4
Ladislav Molnár <ladislav.molnar@gmail.com>
2025-04-12 23:34:02 +0200

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -6,4 +6,4 @@ class HelloConan(ConanFile):
   description = """This is a Hello World library.
                     A fully featured, portable, C++ library to say Hello World in the stdout,
                     with incredible iostreams performance"""
-  requires = "boost/1.86.0", "botan/3.6.1", "date/3.0.3", "glog/0.7.1", "gsl-lite/0.40.0", "rapidjson/cci.20230929", "utf8proc/2.9.0", "zlib/1.3.1"
+  requires = "boost/1.86.0", "botan/3.6.1", "date/3.0.3", "glog/0.7.1", "gsl-lite/0.40.0", "rapidjson/cci.20230929", "utf8proc/2.9.0", "zlib/1.3.1", "hot-formula-parser/3.0.0"
